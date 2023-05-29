4b649532b687fe63fbed9da7a5163d8c7c6f7a9b
Florencia Arias <129863478+cande2323@users.noreply.github.com>
2023-05-28 23:30:49 -0300

    programar
    trabajo

---
+++ Untitled-1.py
@@ -0,0 +1,44 @@
+
+import binascii
+from msilib.schema import SelfReg
+from socket import MSG_MORE
+from typing import Self
+
+from cairo import OPERATOR_DEST_OVER
+
+
+def clasificardatos(self,datos,codtad):
+#Dada un tema y codigo de la tad a la que pertenece 
+# Se almacena cada sensor de la trama 
+# cuando el crc sea correcto 
+
+def operador()
+tamtrama = Self.calcularlong
+crepython=binascii.crc32(datos(OPERATOR_DEST_OVER.tamtrama-4))
+
+if crcArduino == crcpython 
+     apuntador = 0 
+     
+     for sensor in self.sensores:
+         if cod tad == sensor.getcodtad()
+          tamdato = sensor.gettamdato()
+          
+          if (sensor.gettipo().=`digital`)
+            sensor.setdato(self-decodifloat/datos(apuntador):[apuntador + tamdata])
+            
+            
+
+else:
+     sensor.setdato(self_ decodfloat(dato)[(apuntador):(apuntador + tamdato)])
+     apuntador=apuntador + tamdato
+     
+     
+     
+     else:
+    
+    MSG_MORE(`se ha recibido una trama corrupta de la tad`)
+    
+     return -1 
+
+return 0 
+
